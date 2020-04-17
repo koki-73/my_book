c800cd670cfa608f79d7973e5ff3b39890758ec2
koki-73 <kokisasaki.htnh@gmail.com>
2020-04-17 15:09:05 +0900

    add index to books

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_08_130540) do
+ActiveRecord::Schema.define(version: 2020_04_17_060235) do
 
   create_table "action_plans", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2020_04_08_130540) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "author"
+    t.index ["title"], name: "index_books_on_title"
   end
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
