da18dcac925e53644be0e624a0e53bc30db56363
koki-73 <kokisasaki.htnh@gmail.com>
2020-05-28 12:45:05 +0900

    update schema

--- db/migrate/20200408130540_change_datatype_title_of_best_books.rb
+++ db/migrate/20200408130540_change_datatype_title_of_best_books.rb
@@ -1,7 +1,7 @@
 class ChangeDatatypeTitleOfBestBooks < ActiveRecord::Migration[5.2]
   def change
-    change_column :best_books, :best_book_id, 'integer USING CAST(best_book_id AS integer)'
-    change_column :best_books, :second_book_id, 'integer USING CAST(second_book_id AS integer)'
-    change_column :best_books, :third_book_id, 'integer USING CAST(third_book_id AS integer)'
+    change_column :best_books, :best_book_id, :integer
+    change_column :best_books, :second_book_id, :integer
+    change_column :best_books, :third_book_id, :integer
   end
 end
