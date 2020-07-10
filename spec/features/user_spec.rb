a221be43db62342988f3e21c05bb8c1415b8a6ac
koki-73 <kokisasaki.htnh@gmail.com>
2020-07-10 16:04:44 +0900

    add user registration test
    ・新規登録のテスト追加
    ・deviseのビューファイル一部修正

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -1,13 +1,14 @@
 require 'rails_helper'
 
 feature 'user', type: :feature do
+  
   let(:user) { create(:user) }
-
+  
   scenario 'ログインできること' do
     # ログイン前にはログアウトリンクがない
     visit root_path
     expect(page).to have_no_content('ログアウト')
-
+    
     # ログイン処理
     visit new_user_session_path
     fill_in 'user_email', with: user.email
@@ -15,6 +16,21 @@ feature 'user', type: :feature do
     find('input[name="commit"]').click
     expect(current_path).to eq root_path
     expect(page).to have_content('ログアウト')
+  end
+  
+  scenario '新規登録できること' do
+    # ログイン前にはログアウトリンクがない
+    visit root_path
+    expect(page).to have_no_content('ログアウト')
     
+    # 新規登録処理
+    visit new_user_registration_path
+    fill_in 'user_nickname', with: "neko"
+    fill_in 'user_email', with: "neko@gmail.com"
+    fill_in 'user_password', with: "nyannyan"
+    fill_in 'user_password_confirmation', with: "nyannyan"
+    find('input[name="commit"]').click
+    expect(current_path).to eq root_path
+    expect(page).to have_content('ログアウト')
   end
 end
